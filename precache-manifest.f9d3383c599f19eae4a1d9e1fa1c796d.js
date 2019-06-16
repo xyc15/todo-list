8d2f6562f86222eac8daa68c6c3c123769bb8637
Cathy <14879310+xyc15@users.noreply.github.com>
2019-06-15 22:47:21 -0700

    Updates

---
+++ precache-manifest.f9d3383c599f19eae4a1d9e1fa1c796d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cb551f735ef9bef0433a547139527e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2e7f83b2678dfae799dd",
+    "url": "/todo-list/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "56fb879d743d6d77fd12",
+    "url": "/todo-list/static/css/main.d59d24b9.chunk.css"
+  },
+  {
+    "revision": "2e7f83b2678dfae799dd",
+    "url": "/todo-list/static/js/2.561d4953.chunk.js"
+  },
+  {
+    "revision": "56fb879d743d6d77fd12",
+    "url": "/todo-list/static/js/main.f4dd12de.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
